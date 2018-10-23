3621fe8864655d471e76140595ba1e621e9ea21d
kevintudiep <kevintudiep@gmail.com>
2018-10-23 14:42:15 -0500

    completed exercise

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,26 @@
+class PokemonController < ApplicationController
+
+  def index
+  end
+
+  def show
+    res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    body = JSON.parse(res.body)
+    name  = body["name"]
+    id = body["id"]
+    types = body["types"][0]["type"]["name"]
+
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{name}/")
+    body = JSON.parse(res.body)
+    gif_url = body["data"][0]["url"]
+
+
+    render json: { "id": id,
+                   "name": name,
+                   "types": types,
+                   "gif": gif_url   }
+  end
+
+
+
+end
